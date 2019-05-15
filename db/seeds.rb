2e63a90391f5f0925da4f1e7ceb6f8783c6d84ef
Julian Hermann <julian.hermann89@gmail.com>
2019-05-15 19:25:52 +0200

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all
+
+Restaurant.create!(
+  [
+    {
+      name: "Cambusa",
+      address: "Bogenhausen",
+      phone_number: "47283947893",
+      category: "italian"
+    },
+    {
+      name: "Tantris",
+      address: "Schwabing",
+      phone_number: "94789340349",
+      category: "french"
+    },
+    {
+      name: "Hofbräuhaus",
+      address: "Altstadt",
+      phone_number: "983427987398",
+      category: "belgian"
+    },
+    {
+      name: "China Garden",
+      address: "Maxvorstadt",
+      phone_number: "8478992437987",
+      category: "chinese"
+    },
+    {
+      name: "Emiko",
+      address: "Altstadt",
+      phone_number: "4792398479837",
+      category: "japanese"
+    },
+  ]
+)
